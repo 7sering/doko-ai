58aee27c9b06f566430fd672b9f39da695dd7e24
coder <codenow8@gmail.com>
2023-07-20 17:08:07 +0545

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-green-700">
+        Hello Doko AI - SAAS
+      </h1>
+      <Button variant="danger" size="lg">Learn More</Button>
+    </>
+  );
+}

cde3206f85b63354356635184af3d7819a9e591d
coder <codenow8@gmail.com>
2023-07-21 12:35:54 +0545

    folder structure setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-green-700">
+        Dashboard(Protected)
+      </h1>
+    </>
+  );
+}

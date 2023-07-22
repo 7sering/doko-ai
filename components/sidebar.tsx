f2c0ef25e04d975ad3d64b19776f97d1ab1d9e7a
coder <codenow8@gmail.com>
2023-07-22 10:34:42 +0545

    dashboard link activated and reload error fixed in sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
-import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import {
+  Code,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Music,
+  Settings,
+  VideoIcon,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
@@ -14,49 +22,50 @@ const montserrat = Montserrat({
 
 const routes = [
   {
-    label: 'Dashboard',
+    label: "Dashboard",
     icon: LayoutDashboard,
-    href: '/dashboard',
-    color: "text-sky-500"
+    href: "/dashboard",
+    color: "text-sky-500",
   },
   {
-    label: 'Conversation',
+    label: "Conversation",
     icon: MessageSquare,
-    href: '/conversation',
+    href: "/conversation",
     color: "text-violet-500",
   },
   {
-    label: 'Image Generation',
+    label: "Image Generation",
     icon: ImageIcon,
     color: "text-pink-700",
-    href: '/image',
+    href: "/image",
   },
   {
-    label: 'Video Generation',
+    label: "Video Generation",
     icon: VideoIcon,
     color: "text-orange-700",
-    href: '/video',
+    href: "/video",
   },
   {
-    label: 'Music Generation',
+    label: "Music Generation",
     icon: Music,
     color: "text-emerald-500",
-    href: '/music',
+    href: "/music",
   },
   {
-    label: 'Code Generation',
+    label: "Code Generation",
     icon: Code,
     color: "text-green-700",
-    href: '/code',
+    href: "/code",
   },
   {
-    label: 'Settings',
+    label: "Settings",
     icon: Settings,
-    href: '/settings',
+    href: "/settings",
   },
 ];
 
 const SidebarPage = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#011833] text-white">
       <div className="px-3 py-2 flex-1">
@@ -70,7 +79,11 @@ const SidebarPage = () => {
         </Link>
         <div className="space-y-1">
           {routes.map((route) => (
-            <Link key={route.href} href={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}
+            >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
                 {route.label}

7eeace4ab057420ad3da38b4a67d1b8b1ed97d21
coder <codenow8@gmail.com>
2023-08-01 18:56:05 +0545

    loader while result output

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 export const Loader = () => {
-    return(
-        <p>Loading Progress....</p>
-    )
-}
+  return (
+    <div className="h-full flex flex-col gap-y-4 items-center justify-center">
+      <div className="w-10 h-10 animate-spin">
+        <Image alt="logo" fill src="/logo.png" />
+      </div>
+      <p className="text-sm text-muted-foreground">Doko is thinking....</p>
+    </div>
+  );
+};

dafb570e86d4ba85e033115976236ec7e1469cb7
coder <codenow8@gmail.com>
2023-08-04 17:53:02 +0545

    video generation success

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -108,9 +108,12 @@ const VideoPage = () => {
 
             {!video && !isLoading && <Empty label="No Video Generated." />}
             {video && (
-            <video className="w-full aspect-video mt-8 rounded-lg border bg-black">
-                <source src={video} type="video/mp4" />
-            </video>
+              <video
+                controls
+                className="w-full aspect-video mt-8 rounded-lg border bg-black"
+              >
+                <source src={video} />
+              </video>
             )}
           </div>
         </div>

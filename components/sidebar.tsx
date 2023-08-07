85719667faa4c67ec74df8c7d31d0ca192a02823
coder <codenow8@gmail.com>
2023-08-07 07:40:03 +0545

    free credits counter function working fine

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,6 +14,7 @@ import {
   VideoIcon,
 } from "lucide-react";
 import { usePathname } from "next/navigation";
+import FreeCounter from "./free-counter";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -64,7 +65,11 @@ const routes = [
   },
 ];
 
-const SidebarPage = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const SidebarPage = ({ apiLimitCount = 0}: SidebarProps) => {
   const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#011833] text-white">
@@ -82,7 +87,12 @@ const SidebarPage = () => {
             <Link
               key={route.href}
               href={route.href}
-              className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
@@ -92,6 +102,7 @@ const SidebarPage = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
